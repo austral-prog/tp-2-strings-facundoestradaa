558305091b8a20bb0e151abd2d685c9197203745
facundoestradaa <es.facundo.08@gmail.com>
2026-03-18 18:59:35 -0300

    Update names.py

--- names.py
+++ names.py
@@ -3,8 +3,8 @@ def names():
     minúsculas, título, mayúsculas y con tabulador.
     """
     pass
-    nombre=input("Ingrese su nombre")
-    apellido= input("Ingrese su apellido")
+    nombre=input("Ingrese su nombre:")
+    apellido= input("Ingrese su apellido:")
     nombre_completo = nombre + "" + apellido
 
     print(nombre_completo.title())
