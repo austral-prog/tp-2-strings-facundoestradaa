7178c6e809b1cd5ea3fd5d7cc4bfeca95b20b572
facundoestradaa <es.facundo.08@gmail.com>
2026-03-23 18:49:11 -0300

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -4,8 +4,8 @@ def slice_simple():
     """
     texto = "Awesome"
 
-    print((texto[0:3]).lower())
-    print((texto[2:5]).lower())
+    print(texto[0:3].lower())
+    print(texto[2:5])
 
     print(texto.lower())
 
