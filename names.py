a98948c52f028a8cc1770b12df0462d2fca009d0
facundoestradaa <es.facundo.08@gmail.com>
2026-03-23 18:24:18 -0300

    Update names.py

--- names.py
+++ names.py
@@ -3,17 +3,16 @@ def names():
     minúsculas, título, mayúsculas y con tabulador.
     """
 
-    nombre=input()
-    apellido= input()
+    nombre = input()
+    apellido = input()
 
-    nombre_minuscula =(nombre.lower() + " " + apellido.lower())
-    nombre_iniciales = (nombre.title() + " " + apellido.title())
-    nombre_mayuscula = (nombre.upper() + " " + apellido.upper())
+    nombre_minuscula = nombre.lower() + " " + apellido.lower()
+    nombre_iniciales = nombre.title() + " " + apellido.title()
+    nombre_mayuscula = nombre.upper() + " " + apellido.upper()
 
     print(nombre_minuscula)
     print(nombre_iniciales)
     print(nombre_mayuscula)
-
     print(f"\t{nombre_minuscula}")
 
 
