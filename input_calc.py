72170ff2935c184f3bb9d983549682ff7694a0fd
facundoestradaa <es.facundo.08@gmail.com>
2026-03-23 19:37:58 -0300

    Update input_calc.py

--- input_calc.py
+++ input_calc.py
@@ -4,14 +4,10 @@ def rectangle():
     """
     base= int(input())
     altura= int(input())
-
     print(f"Base: {base}")
     print(f"Altura: {altura}")
-
-    
     area_resultado = base * altura
     perimetro_resultado= (base * 2) + (altura * 2)
-
     print(f"Area: {area_resultado}")
     print(f"Perimetro: {perimetro_resultado}")
 
