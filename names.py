71a1e06b06c29f88794ca50b2c0693ecdc6b1c40
facundoestradaa <es.facundo.08@gmail.com>
2026-03-24 13:39:04 -0300

    Update names.py

--- names.py
+++ names.py
@@ -2,19 +2,13 @@ def names():
     """Lee nombre y apellido, e imprime el nombre completo en distintos formatos:
     minúsculas, título, mayúsculas y con tabulador.
     """
-
+    pass
     nombre = input()
     apellido = input()
-    nombre_minuscula = nombre.lower() + " " + apellido.lower()
-    nombre_iniciales = nombre.title() + " " + apellido.title()
-    nombre_mayuscula = nombre.upper() + " " + apellido.upper()
-    print(nombre_minuscula)
-    print(nombre_iniciales)
-    print(nombre_mayuscula)
-    print(f"\t{nombre_minuscula}")
+    nombre_completo = (f"{nombre} {apellido}")
 
-
-
-
-
+    print(f"{nombre_completo.lower()}")
+    print(f"{nombre_completo.title()}")
+    print(f"{nombre_completo.upper()}")
+    print(f"\t{nombre_completo.lower()}")
 
