cb9f6f65cd9a7ea5d803f743a29c5c3a57370b3d
facundoestradaa <es.facundo.08@gmail.com>
2026-03-24 16:30:57 -0300

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -2,8 +2,7 @@ def slice_simple():
     """Dado el texto 'Awesome', imprime distintos substrings
     usando slicing y lower().
     """
-    texto = "Awesome"
-    print(texto[0:3].lower())
+    texto= "Awesome"
+    print(texto[:3].lower())
     print(texto[2:5])
     print(texto.lower())
-
